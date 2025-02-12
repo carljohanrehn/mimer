290ec3aeaa72498332e46950faf99e5325c66ad4
carljohanrehn <care02@gmail.com>
2025-02-12 14:37:29 +0100

    Removed 'config' from version control and updated .gitignore, and other project related files

--- setup.py
+++ setup.py
@@ -1,15 +1,3 @@
-from setuptools import setup, find_packages
+from setuptools import setup
 
-setup(
-    name="mimer",
-    version="0.1.0",
-    packages=find_packages(),  # Finds all packages inside your project
-    include_package_data=True,  # Ensures data files declared in MANIFEST.in are included
-    package_data={
-        "": [  # Use an empty string to apply the pattern to ALL packages
-            "config/**",  # Add all files inside the `config` directory
-            "chroma_storage/**",  # Add all files inside `chroma_storage`
-            "database/**",  # Add all files inside `database`
-        ],
-    },
-)
+setup()
